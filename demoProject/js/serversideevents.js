25b4026c38dd793cae027a5232bd6c9bbd8db32c
SK <sk@gmail.com>
2015-05-07 16:21:09 +0530

    Update after peer review

--- demoProject/js/serversideevents.js
+++ demoProject/js/serversideevents.js
@@ -2,9 +2,9 @@ window.onload = function() {
 if(typeof(EventSource) !== "undefined") {
     var source = new EventSource("/stream");
 	source.onmessage = function(event) {
-        document.getElementById("result").innerHTML += event.data + "<br>";
+        document.getElementById("scrollableresult").innerHTML += event.data + "<br>";
     };
 } else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+    document.getElementById("scrollableresult").innerHTML = "Sorry, your browser does not support server-sent events...";
 }
 }
