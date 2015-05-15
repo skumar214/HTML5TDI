feda0caa19d6ad9c17bb566855dd455b4e29182a
SK <sk@gmail.com>
2015-05-15 11:29:50 +0530

    Updated Code

--- demoProject/js/geolocation.js
+++ demoProject/js/geolocation.js
@@ -2,6 +2,7 @@ var x ;
 
 function init(){
 	x = document.getElementById("demo");
+	
 }
 
 function getLocation() {
@@ -13,14 +14,24 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;	
+	var loc = {};
+	loc.lat =  position.coords.latitude;
+	loc.lng = position.coords.longitude;
+	loc.acc = position.coords.accuracy;
+
+
+    x.innerHTML = "Latitude: " + loc.lat + 
+    "<br>Longitude: " + loc.lng +
+    "<br>Accuracy (meters): " + loc.acc;
 	
-	var latlon = position.coords.latitude + "," + position.coords.longitude;
-	
-	var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
-    +latlon+"&zoom=14&size=400x300&sensor=false";
-    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+	var myOptions = {zoom:14,center:new google.maps.LatLng(loc.lat,loc.lng),mapTypeId: google.maps.MapTypeId.TERRAIN}; 
+	var map = new google.maps.Map(document.getElementById("mapholder"), myOptions); 
+	var marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(loc.lat, loc.lng)}); 
+	var infowindow = new google.maps.InfoWindow({content:"<b>You are here!</b>" }); 
+	google.maps.event.addListener(marker, "click", function(){
+		infowindow.open(map,marker); 	
+		}); 
+	infowindow.open(map,marker); 
 }
 
 function showError(error) {
@@ -39,3 +50,34 @@ function showError(error) {
             break;
     }
 }
+//google.load("maps", "3.x", {other_params: "sensor=false", callback:getClientLocation});
+//Google API Way
+function getClientLocation(){
+			
+		    var geocoder = new google.maps.Geocoder();
+    		if(google.loader.ClientLocation) {
+        	var loc = {};
+        	loc.lat=google.loader.ClientLocation.latitude;
+        	loc.lng = google.loader.ClientLocation.longitude;
+
+			var myOptions = {zoom:14,center:new google.maps.LatLng( loc.lat,loc.lng),mapTypeId: google.maps.MapTypeId.TERRAIN}; 
+			var map = new google.maps.Map(document.getElementById("mapholderclient"), myOptions); 
+			var marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(loc.lat, loc.lng)}); 
+			var infowindow = new google.maps.InfoWindow({content:"<b>You are here!</b>" }); 
+			google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker); 
+			}); 
+
+			infowindow.open(map,marker); 
+			document.getElementById("lat").innerHTML ="Latitude: "+ loc.lat +'<br>Longitude: '+ loc.lng;
+        	
+    		}
+    		else{
+			document.getElementById("lat").innerHTML ="Cannot determine your location";
+    		}
+			
+			
+			}
+	
+//google.maps.event.addDomListener(window, 'load', init_map); 
+//	var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="+loc.lat+","+loc.lng+"&zoom=14&size=400x300&sensor=false";
+//	document.getElementById("mapholderclient").innerHTML = "<img src='"+img_url+"'>";
